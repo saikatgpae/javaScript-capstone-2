043e34d685689d3d4e14bdf2b825b5bb71009e01
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-13 19:30:15 +0530

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+const footer = () => {
+  const footer = document.createElement('footer');
+  footer.classList = 'footer';
+  footer.innerHTML = 'Created by Microverse under the CC license';
+  document.body.appendChild(footer);
+};
+
+export default footer;
