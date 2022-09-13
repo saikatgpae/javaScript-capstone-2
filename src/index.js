ed52ac367815a7822127158f154180e51950424a
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-13 16:03:17 +0530

    Remove linters error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
+import Icon from './images/icon.png';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
-   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
   // Add the image to our existing div.
   const myIcon = new Image();
@@ -16,7 +16,7 @@ import Icon from './icon.png';
 
   element.appendChild(myIcon);
 
-   return element;
- }
+  return element;
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
