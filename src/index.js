dc97c40f96a094f0c5cf0ad9bc6db57522b55b1b
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-14 13:44:14 +0530

    Remove unnecessary commented line from index.js.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import header from './components/header.js';
 import loadAll from './components/loadAll.js';
-// import newApp from './components/newApp.js';
 import './style.css';
 
 // PRINT ALL MEALS.
 loadAll();
-
-// CREATE A NEW APP
-// newApp();
-
-// header();
