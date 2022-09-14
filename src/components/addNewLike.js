4829e81b95d79f971df16a86716e7083724e33fe
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-14 20:40:48 +0530

    Add newlike.

--- src/components/addNewLike.js
+++ src/components/addNewLike.js
@@ -1,5 +1,13 @@
-const addNewLikes = () => {
-  console.log('hello');
+const addNewLikes = async (e) => {
+  e.preventDefault();
+  const itemId = e.currentTarget.parentNode.parentNode.parentNode.childNodes[4].childNodes[0].id.split('-')[1];
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/BVDQ4KJUQMbnNVhecGTj/likes/', {
+    method: 'POST',
+    body: JSON.stringify({ item_id: itemId }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  })
+    .then((response) => response.text());
+  // window.location.reload();
 };
 
 export default addNewLikes;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import loadAll from './components/loadAll.js';
 import './style.css';
-import addNewLikes from './components/addNewLike.js';
+import allLikes from './components/allLikes.js';
+
+// PRINT ALL LIKES
+allLikes();
 
 // PRINT ALL MEALS.
 loadAll();
-
-// ADD NEW LIKES
-
-// window.addEventListener('DOMContentLoaded', () => {
-//   document.querySelectorAll('.like').forEach((item) => {
-//     item.addEventListener('click', addNewLikes);
-//   });
-// });
