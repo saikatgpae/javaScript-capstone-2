0f7fb049483883f17eea0d6972caee2eb67354a7
unknown <okonjogilbert2@gmail.com>
2022-09-14 12:16:40 +0300

    Add header

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import _ from 'lodash';
 import './style.css';
-import Icon from './images/icon.png';
+import header from './components/header.js';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
+header();
