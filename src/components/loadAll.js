4640b8f1fa9164ef65a634008783a1a68480f85e
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-14 14:16:04 +0530

    Add margin top to the meal name.

--- src/components/loadAll.js
+++ src/components/loadAll.js
@@ -10,7 +10,7 @@ const loadAll = async () => {
     const meal = document.createElement('div');
     meal.className = 'p-4 m-4 meal';
     meal.innerHTML = `<div><img class='mealImage' src="${element.strMealThumb}" alt="meal image"></div>
-    <div class="mealname"><strong>${element.strMeal}</strong><a href=""><img class="like" src="${like}" alt="heart shape image"></a></div>
+    <div class="mealname mt-4"><strong>${element.strMeal}</strong><a href=""><img class="like" src="${like}" alt="heart shape image"></a></div>
     <div class="mt-3 d-flex comments-button"><button class="comments" id="coment-${element.idMeal}">Comments</button></div>
     <div class="mt-2 d-flex reservatio-button"><button class="reservation" id="reservation-${element.idMeal}">Reservation</button></div>`;
     container.appendChild(meal);
