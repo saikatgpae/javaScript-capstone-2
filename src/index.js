312f4f618743cd76ab05b90ff45c78e37c0d138e
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-14 18:25:26 +0530

    Fixed linters.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import footer from './components/footer.js';
-
-// PRINT THE FOOTER
-footer();
-
 import loadAll from './components/loadAll.js';
-import './style.css';
 
 // PRINT ALL MEALS.
 loadAll();
+
+// PRINT THE FOOTER
+footer();
